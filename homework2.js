530b6a0be8addc3fd7ad06b50c0e84c27f118569
Tigranian <31800260+Tigranian@users.noreply.github.com>
2017-09-12 22:36:41 +0400

    Create homework2.js

---
+++ homework2.js
@@ -0,0 +1,15 @@
+const number13 = 13;
+const number26 = 26;
+const name = "Arman Tigranian";
+
+const result1 = number13 + number26;
+const result2 = number13 * number26;
+const result3 = number13 / number26;
+
+console.log(number13*number26)
+console.log(number13/number26)
+console.log(number13-number26)
+console.log(number13+number26)
+console.log(number13+name)
+
+
